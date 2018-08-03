84de3a0f54ff9b1b9d8a99cc95ee110103b4cb0c
Tch123456 <41766861+Tch123456@users.noreply.github.com>
2018-08-03 14:54:03 +0800

    新增批量修改设备桩号的窗体

---
+++ Propert/FEupdatePileNo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowMake.Propert
+{
+    public partial class FEupdatePileNo : Form
+    {
+        public FEupdatePileNo()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
